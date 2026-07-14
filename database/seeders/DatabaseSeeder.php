8dd6d32091352212c4f8f4e9b0ce659b96fbc852
Heru Rizky Fajar <herurizkyfajar@gmail.com>
2026-07-14 20:44:44 +0700

    fix: use local product images from /produk folder instead of external URLs

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -45,16 +45,16 @@ class DatabaseSeeder extends Seeder
 
         // Pisang Segar
         $pisangProducts = [
-            ['name' => 'Pisang Ambon Lumut', 'slug' => 'pisang-ambon-lumut', 'image' => 'https://rizkypisang.com/wp-content/uploads/2025/10/Desain-tanpa-judul-2025-10-24T223457.064.png'],
-            ['name' => 'Pisang Ambon Putih', 'slug' => 'pisang-ambon-putih', 'image' => 'https://rizkypisang.com/wp-content/uploads/2025/10/Desain-tanpa-judul-2025-10-24T223143.124.png'],
-            ['name' => 'Pisang Raja Cere', 'slug' => 'pisang-raja-cere', 'image' => 'https://rizkypisang.com/wp-content/uploads/2025/10/Desain-tanpa-judul-2025-10-24T222844.320.png'],
-            ['name' => 'Pisang Muli', 'slug' => 'pisang-muli', 'image' => 'https://rizkypisang.com/wp-content/uploads/2025/10/Desain-tanpa-judul-2025-10-24T222654.511.png'],
-            ['name' => 'Pisang Siem', 'slug' => 'pisang-siem', 'image' => 'https://rizkypisang.com/wp-content/uploads/2025/10/Desain-tanpa-judul-2025-10-24T222449.495.png'],
-            ['name' => 'Pisang Kepok', 'slug' => 'pisang-kepok', 'image' => 'https://rizkypisang.com/wp-content/uploads/2025/10/Desain-tanpa-judul-2025-10-24T222302.180.png'],
-            ['name' => 'Pisang Bangkawulu', 'slug' => 'pisang-bangkawulu', 'image' => 'https://rizkypisang.com/wp-content/uploads/2025/10/Desain-tanpa-judul-2025-10-24T221940.571.png'],
-            ['name' => 'Pisang Nangka', 'slug' => 'pisang-nangka', 'image' => 'https://rizkypisang.com/wp-content/uploads/2025/10/Desain-tanpa-judul-2025-10-24T221629.265.png'],
-            ['name' => 'Pisang Raja Bulu', 'slug' => 'pisang-raja-bulu', 'image' => 'https://rizkypisang.com/wp-content/uploads/2025/10/Desain-tanpa-judul-2025-10-24T221324.893.png'],
-            ['name' => 'Pisang Kapas', 'slug' => 'pisang-kapas', 'image' => 'https://rizkypisang.com/wp-content/uploads/2025/10/Desain-tanpa-judul-2025-10-24T220912.281.png'],
+            ['name' => 'Pisang Ambon Lumut', 'slug' => 'pisang-ambon-lumut', 'image' => '/produk/Desain-tanpa-judul-2025-10-24T223457.064.png'],
+            ['name' => 'Pisang Ambon Putih', 'slug' => 'pisang-ambon-putih', 'image' => '/produk/Desain-tanpa-judul-2025-10-24T223143.124.png'],
+            ['name' => 'Pisang Raja Cere', 'slug' => 'pisang-raja-cere', 'image' => '/produk/Desain-tanpa-judul-2025-10-24T222844.320.png'],
+            ['name' => 'Pisang Muli', 'slug' => 'pisang-muli', 'image' => '/produk/Desain-tanpa-judul-2025-10-24T222654.511.png'],
+            ['name' => 'Pisang Siem', 'slug' => 'pisang-siem', 'image' => '/produk/Desain-tanpa-judul-2025-10-24T222449.495.png'],
+            ['name' => 'Pisang Kepok', 'slug' => 'pisang-kepok', 'image' => '/produk/Desain-tanpa-judul-2025-10-24T222302.180.png'],
+            ['name' => 'Pisang Bangkawulu', 'slug' => 'pisang-bangkawulu', 'image' => '/produk/Desain-tanpa-judul-2025-10-24T221940.571.png'],
+            ['name' => 'Pisang Nangka', 'slug' => 'pisang-nangka', 'image' => '/produk/Desain-tanpa-judul-2025-10-24T221629.265.png'],
+            ['name' => 'Pisang Raja Bulu', 'slug' => 'pisang-raja-bulu', 'image' => '/produk/Desain-tanpa-judul-2025-10-24T221324.893.png'],
+            ['name' => 'Pisang Kapas', 'slug' => 'pisang-kapas', 'image' => '/produk/Desain-tanpa-judul-2025-10-24T220912.281.png'],
         ];
 
         foreach ($pisangProducts as $i => $product) {
@@ -67,9 +67,9 @@ class DatabaseSeeder extends Seeder
 
         // Pisang Olahan
         $olahanProducts = [
-            ['name' => 'Keripik Pisang', 'slug' => 'keripik-pisang', 'image' => 'https://rizkypisang.com/wp-content/uploads/2025/10/Rizky-Pisang-4.png'],
-            ['name' => 'Sale Pisang Matang', 'slug' => 'sale-pisang-matang', 'image' => 'https://rizkypisang.com/wp-content/uploads/2025/10/Rizky-Pisang-2.png'],
-            ['name' => 'Sale Pisang Mentah', 'slug' => 'sale-pisang-mentah', 'image' => 'https://rizkypisang.com/wp-content/uploads/2025/10/Rizky-Pisang-1.png'],
+            ['name' => 'Keripik Pisang', 'slug' => 'keripik-pisang', 'image' => '/produk/Rizky-Pisang-4.png'],
+            ['name' => 'Sale Pisang Matang', 'slug' => 'sale-pisang-matang', 'image' => '/produk/Rizky-Pisang-2.png'],
+            ['name' => 'Sale Pisang Mentah', 'slug' => 'sale-pisang-mentah', 'image' => '/produk/Rizky-Pisang-1.png'],
         ];
 
         foreach ($olahanProducts as $i => $product) {
@@ -82,16 +82,16 @@ class DatabaseSeeder extends Seeder
 
         // Sembako & Lainnya
         $sembakoProducts = [
-            ['name' => 'Gas 3Kg', 'slug' => 'gas-3kg', 'image' => 'https://rizkypisang.com/wp-content/uploads/2025/10/Rizky-Pisang-16.jpg'],
-            ['name' => 'Aneka Bumbu Dapur', 'slug' => 'aneka-bumbu-dapur', 'image' => 'https://rizkypisang.com/wp-content/uploads/2025/10/Rizky-Pisang-15.jpg'],
-            ['name' => 'Aneka Gula Merah', 'slug' => 'aneka-gula-merah', 'image' => 'https://rizkypisang.com/wp-content/uploads/2025/10/Rizky-Pisang-13.jpg'],
-            ['name' => 'Aneka Umbi', 'slug' => 'aneka-umbi', 'image' => 'https://rizkypisang.com/wp-content/uploads/2025/10/Rizky-Pisang-14.jpg'],
-            ['name' => 'Arang Kayu', 'slug' => 'arang-kayu', 'image' => 'https://rizkypisang.com/wp-content/uploads/2025/10/Rizky-Pisang-11.jpg'],
-            ['name' => 'Arang Batok Kelapa', 'slug' => 'arang-batok-kelapa', 'image' => 'https://rizkypisang.com/wp-content/uploads/2025/10/Rizky-Pisang-10.jpg'],
-            ['name' => 'Batok Kelapa', 'slug' => 'batok-kelapa', 'image' => 'https://rizkypisang.com/wp-content/uploads/2025/10/Rizky-Pisang-9.jpg'],
-            ['name' => 'Santan Kelapa', 'slug' => 'santan-kelapa', 'image' => 'https://rizkypisang.com/wp-content/uploads/2025/10/Rizky-Pisang-8.jpg'],
-            ['name' => 'Kelapa Parud', 'slug' => 'kelapa-parud', 'image' => 'https://rizkypisang.com/wp-content/uploads/2025/10/Rizky-Pisang-7.jpg'],
-            ['name' => 'Kelapa Sayur Butiran', 'slug' => 'kelapa-sayur-butiran', 'image' => 'https://rizkypisang.com/wp-content/uploads/2025/10/Rizky-Pisang-6.jpg'],
+            ['name' => 'Gas 3Kg', 'slug' => 'gas-3kg', 'image' => '/produk/Rizky-Pisang-16.jpg'],
+            ['name' => 'Aneka Bumbu Dapur', 'slug' => 'aneka-bumbu-dapur', 'image' => '/produk/Rizky-Pisang-15.jpg'],
+            ['name' => 'Aneka Gula Merah', 'slug' => 'aneka-gula-merah', 'image' => '/produk/Rizky-Pisang-13.jpg'],
+            ['name' => 'Aneka Umbi', 'slug' => 'aneka-umbi', 'image' => '/produk/Rizky-Pisang-14.jpg'],
+            ['name' => 'Arang Kayu', 'slug' => 'arang-kayu', 'image' => '/produk/Rizky-Pisang-11.jpg'],
+            ['name' => 'Arang Batok Kelapa', 'slug' => 'arang-batok-kelapa', 'image' => '/produk/Rizky-Pisang-10.jpg'],
+            ['name' => 'Batok Kelapa', 'slug' => 'batok-kelapa', 'image' => '/produk/Rizky-Pisang-9.jpg'],
+            ['name' => 'Santan Kelapa', 'slug' => 'santan-kelapa', 'image' => '/produk/Rizky-Pisang-8.jpg'],
+            ['name' => 'Kelapa Parud', 'slug' => 'kelapa-parud', 'image' => '/produk/Rizky-Pisang-7.jpg'],
+            ['name' => 'Kelapa Sayur Butiran', 'slug' => 'kelapa-sayur-butiran', 'image' => '/produk/Rizky-Pisang-6.jpg'],
         ];
 
         foreach ($sembakoProducts as $i => $product) {
@@ -106,7 +106,7 @@ class DatabaseSeeder extends Seeder
         Product::create([
             'name' => 'Ikan Lele',
             'slug' => 'ikan-lele',
-            'image' => 'https://rizkypisang.com/wp-content/uploads/2025/12/Rizky-Pisang-19.jpg',
+            'image' => '/produk/Rizky-Pisang-19.jpg',
             'category_id' => $perikanan->id,
             'sort_order' => 1,
         ]);
